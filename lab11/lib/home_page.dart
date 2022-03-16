f9a60ac83de016d4d4b035cc16d2d935db8afb0c
guitar-12022000 <preyanuch.ph@kkumail.com>
2022-03-16 21:01:11 +0700

    initial commit

---
+++ lab11/lib/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                Image(
+                    image: NetworkImage(
+                        'https://th.bing.com/th/id/OIP.ZIQweApYyeryI4riM0YLHQHaF6?pid=ImgDet&rs=1')),
+                Text(
+                  "Welcome to The Quiz App",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Text(
+                  "By Preyanuch Phutsay 623040275-5",
+                  style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/quiz");
+                },
+                child: const Text("Start")),
+          ),
+        ],
+      ),
+    );
+  }
+}
